1d4530d9455b278b193a3040b49a836316522066
alejandrakn <alejandra.cussianovi@utec.edu.pe>
2026-09-22 11:40:15 -0500

    Delete src directory

--- src/api/ms1.js
+++
@@ -1,6 +0,0 @@
-import { MS1_URL, fetchJson } from "./config";
-
-export const ms1 = {
-  listarClientes: () => fetchJson(`${MS1_URL}/clientes`),
-  listarPedidos: () => fetchJson(`${MS1_URL}/pedidos`),
-};
--- src/api/ms3.js
+++
@@ -1,6 +0,0 @@
-import { MS3_URL, fetchJson } from "./config";
-
-export const ms3 = {
-  listarMesas: () => fetchJson(`${MS3_URL}/mesas`),
-  listarReservas: () => fetchJson(`${MS3_URL}/reservas`),
-};
